6adef187b0a252d88fc5d1f563b25d2a4ab89a2b
Deep Sharma <sharmadeep0301@gmail.com>
2025-04-08 14:23:10 +0530

    smallc hange

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['media.istockphoto.com', 'developers.google.com', 'lh3.googleusercontent.com', 'via.placeholder.com'],
+    domains: [
+      'media.istockphoto.com',
+      'developers.google.com',
+      'lh3.googleusercontent.com',
+      'via.placeholder.com',
+    ],
+  },
+  eslint: {
+    // Ignore ESLint errors during builds (optional, use with caution)
+    ignoreDuringBuilds: true,
   },
 };
 
