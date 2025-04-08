fc0445cec215eaa15f3c0c444573166f8a06e432
Deep Sharma <sharmadeep0301@gmail.com>
2025-04-08 14:11:44 +0530

    changed the api URL

--- graphql/graphqlClient.ts
+++ graphql/graphqlClient.ts
@@ -3,7 +3,7 @@ import { GraphQLClient } from 'graphql-request';
 export const getGraphQLClient = () => {
   const token = typeof window !== 'undefined' ? localStorage.getItem('jwt_token') : null;
 
-  return new GraphQLClient('https://d2aewxzretzu52.cloudfront.net/ghraphql', {
+  return new GraphQLClient('https://d2t6f6rfgd2p81.cloudfront.net/graphql', {
     headers: token ? { Authorization: `Bearer ${token}` } : {},
   });
 };
