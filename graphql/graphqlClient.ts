802d75fd6468f033e0c644a68928b95c8e3ccd2f
Deep Sharma <sharmadeep0301@gmail.com>
2025-04-08 14:16:21 +0530

    change

--- graphql/graphqlClient.ts
+++ graphql/graphqlClient.ts
@@ -3,7 +3,7 @@ import { GraphQLClient } from 'graphql-request';
 export const getGraphQLClient = () => {
   const token = typeof window !== 'undefined' ? localStorage.getItem('jwt_token') : null;
 
-  return new GraphQLClient('https://d2t6f6rfgd2p81.cloudfront.net/graphql', {
+  return new GraphQLClient(process.env.NEXT_PUBLIC_API_URL as string, {
     headers: token ? { Authorization: `Bearer ${token}` } : {},
   });
 };
